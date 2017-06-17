cfba7b2df0eb23a382b04a2e8aafa931f893bece
Jason Cao <jc6933@nyu.edu>
2017-06-17 11:14:15 +0200

    t

---
+++ trial.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pickle
+import math
+import scipy
+import matplotlib.pyplot as plt
+from termcolor import colored
+import pickle
+import matplotlib
+import emcee
+import scipy.optimize as op
+
+
+print("check")
